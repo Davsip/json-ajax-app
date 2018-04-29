4ad2c128542fba76a8a6e333c71da15245c2bf95
davsip <david.sipala@gmail.com>
2018-04-29 09:56:19 -0500

    first add

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+var myDog = {
+
+    "name" : " barksalot",
+    "species" : "dog",
+     "favFood" : "meat"
+}
+
+myDog.favFood // how to acces a property 
+
+
+
+var myFavColors = ["red", "blue", "black","orange" ];     // arrays 
+
+myFavColors[0];
+console.log (myFavColors);
