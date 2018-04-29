5ddb63432ab1229c25d15992d884d268bc59540a
davsip <david.sipala@gmail.com>
2018-04-29 10:31:48 -0500

    adding js

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,5 @@
-var myDog = {
-
-    "name" : " barksalot",
-    "species" : "dog",
-     "favFood" : "meat"
-}
-
-myDog.favFood // how to acces a property 
-
-
-
-var myFavColors = ["red", "blue", "black","orange" ];     // arrays 
-
-myFavColors[0];
-console.log (myFavColors);
+var ourRequest = new XMLHttpRequest();         // XMLHttpRequest <- this tool will stablish a connection with a URL that we specify and let us send or receive data 
+ ourRequest.open("GET", 'https://learnwebcode.github.io/json-example/animals-1.json'); // we need to use a varibale to use a method names open to specify we use ourRequest.open() // GET is to get data from the url tha e want
+ ourRequest.onload = function() {    // method called "onload",  using an anonymous function 
+     console.log(ourRequest.responseText);   // log it to the console 
+ };
